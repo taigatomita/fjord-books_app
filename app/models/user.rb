652a65428ed5e773f94a2aa3d2941a88faaba359
taigatomita <tiger101837@gmail.com>
2021-10-27 13:25:13 +0900

    ユーザーフォロー機能を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,29 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  has_many :follows, dependent: :destroy
+  has_many :followings, through: :follows, source: :follow
+
+  has_many :reverse_of_follows, class_name: 'Follow', foreign_key: 'follow_id', dependent: :destroy, inverse_of: :follow
+  has_many :followers, through: :reverse_of_follows, source: :user
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   has_one_attached :avatar
+
+  def follow(other_user)
+    return if self == other_user
+
+    follows.find_or_create_by(follow_id: other_user.id)
+  end
+
+  def unfollow(other_user)
+    friendship = follows.find_by(follow_id: other_user.id)
+    friendship&.destroy
+  end
+
+  def following?(other_user)
+    followings.include?(other_user)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_31_231050) do
+ActiveRecord::Schema.define(version: 2021_10_20_140046) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -35,7 +35,7 @@ ActiveRecord::Schema.define(version: 2021_05_31_231050) do
   end
 
   create_table "active_storage_variant_records", force: :cascade do |t|
-    t.bigint "blob_id", null: false
+    t.integer "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
@@ -47,6 +47,16 @@ ActiveRecord::Schema.define(version: 2021_05_31_231050) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "author"
     t.string "picture"
+  end
+
+  create_table "follows", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "follow_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["follow_id"], name: "index_follows_on_follow_id"
+    t.index ["user_id", "follow_id"], name: "index_follows_on_user_id_and_follow_id", unique: true
+    t.index ["user_id"], name: "index_follows_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -67,4 +77,6 @@ ActiveRecord::Schema.define(version: 2021_05_31_231050) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "follows", "users"
+  add_foreign_key "follows", "users", column: "follow_id"
 end
