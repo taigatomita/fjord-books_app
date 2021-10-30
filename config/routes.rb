3872d59e09513ed791dee4f45cb1b21a5b6d2572
taigatomita <tiger101837@gmail.com>
2021-10-30 15:09:41 +0900

    余分なルートを削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
-  devise_for :users, controllers: {
-    registrations: 'users/registrations'
-  }
-  resources :users
+  devise_for :users, controllers: { registrations: 'users/registrations' }
+  resources :users, only: %i[index show]
   root to: 'books#index'
   resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
